89a712cd926eb08d0d491a618bddadaf486e1881
mayor167 <adenijimayokun@gmail.com>
2025-05-21 09:00:29 +0100

    card for the details of upcoming flight is added on the home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:booktickets/screens/ticket_view.dart';
 import 'package:booktickets/utilis/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,6 +17,7 @@ class HomeScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20),
             child:Column(
               children: [
+                const Gap(20),
                 Row(  
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children:[
@@ -21,6 +25,7 @@ class HomeScreen extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text('Good morning',style: Styles.headLineStyle3,),
+                        const Gap(1),
                         Text('Book Tickets',style: Styles.headLineStyle1,),
                       ],
                     ),
@@ -38,11 +43,39 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                   ],
-                )
+                ),
+                const Gap(10),
+                Container(
+                  // height: 40,
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                   decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFFF4F6FD),
+                   ),
+                  child: Row(
+                    children: [
+                         const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
+                         Text("Search", style: Styles.headLineStyle4,),
+                    ],
+                  ),
+                ),
+               const Gap(20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Up Coming Events", style: Styles.headLineStyle2,),
+                    InkWell(
+                      onTap: (){
+                        print('You are tapped');
+                      },
+                      child: Text("View all", style: Styles.textStyle.copyWith(color:Styles.primaryColor),))
+                  ],
+                ),
               ],
             ),
-          )
-
+          ),
+          const Gap(15),
+          TicketView(),
         ],
       )
       );
--- lib/utilis/app_styles.dart
+++ lib/utilis/app_styles.dart
@@ -9,7 +9,7 @@ static Color orangeColor = const Color(0xFF526799);
 static TextStyle textStyle = TextStyle(fontSize: 16, color:textColor, fontWeight:FontWeight.w500 );
 static TextStyle headLineStyle1 = TextStyle(fontSize: 26, color:textColor, fontWeight:FontWeight.bold );
 static TextStyle headLineStyle2 = TextStyle(fontSize: 21, color: textColor, fontWeight: FontWeight.bold);
-static TextStyle headLineStyle3 = TextStyle(fontSize: 17, color: textColor, fontWeight: FontWeight.w500);
+static TextStyle headLineStyle3 = TextStyle(fontSize: 17, color: Colors.grey.shade500, fontWeight: FontWeight.w500);
 static TextStyle headLineStyle4 = TextStyle(fontSize: 14, color: Colors.grey.shade500, fontWeight: FontWeight.w500);
 
 }
